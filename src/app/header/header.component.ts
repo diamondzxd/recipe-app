59ecf8b83fdc2849361bb19e0a598cc831d073f8
Piyush Dhall <dhall.piyush@gmail.com>
2021-08-15 19:30:37 +0530

    Till Routing Module

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-header',
@@ -8,12 +8,6 @@ import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 export class HeaderComponent implements OnInit
 {
   collapsed = true;
-  @Output() featureSelected = new EventEmitter<string>();
-
-  onSelect(feature: string)
-  {
-    this.featureSelected.emit(feature);
-  }
 
   constructor()
   {
